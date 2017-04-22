ca9789b5cd43cd359e83779991693bb4a2c490d5
Anuj Bansal <bansalanuj1996@gmail.com>
2017-04-23 01:35:25 +0530

    Added https support

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ function doneWorking() {
 
 app.run(function () {
   var tag = document.createElement('script');
-  tag.src = "http://www.youtube.com/iframe_api";
+  tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
